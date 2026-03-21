f6bea4c72ebf8bf3a8db4f0f658ac20f9fc47800
ParthRKhakre <parthrkhakre@gmail.com>
2026-03-21 22:27:18 +0530

    .

--- CPP/5.1-BinarySearch/6-First_Last.cpp
+++ CPP/5.1-BinarySearch/6-First_Last.cpp
@@ -1,5 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<int> OccuranceRange(vector<int> &arr,int target,int n){
+
+    int start= -1,end = -1,low = 0,high= n-1;
+
+    while(low<=high){
+        if(arr[low] == target){
+            start = low;
+            break;
+        }else low++;
+    }
+
+    while(low<=high){
+        if(arr[high] == target){
+            end = high;
+            break;
+        }else high--;
+    }
+    
+    return {start,end};
+}
+
 int main(){
+
+    int n,target;
+    cin>>n>>target;
+
+    vector<int> arr(n);
+
+    for(int i = 0;i<n;i++)
+        cin>>arr[i];
+
+    vector<int> range = OccuranceRange(arr,target,n);
+
+    for(auto it:range)
+        cout<<it<<" ";
     
 }
