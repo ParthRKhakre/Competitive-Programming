d6e6c31961bb6b99fbf4a47accc17e6aa1d7402b
ParthRKhakre <parthrkhakre@gmail.com>
2026-03-26 17:01:34 +0530

    koko

--- CPP/5.1-BinarySearch/BS On Answer/3-Koko_eating_banana.cpp
+++ CPP/5.1-BinarySearch/BS On Answer/3-Koko_eating_banana.cpp
@@ -1,10 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int koko(vector<int>&num,int h){
-
+int maximumElement(vector<int> &piles){
+    int maximum = INT_MIN;
+    for(int i = 0; i < piles.size(); i++)
+        maximum = max(maximum,piles[i]);
     
+    return maximum;
+}
 
+int totalHours(vector<int>& piles,int k) {
+    int total = 0;
+    for(int i = 0;i < piles.size();i++){
+        int add = ceil((float)piles[i]/k);
+        total += add;
+    }
+        return total;
+}
+
+int koko(vector<int>& piles, int h) {
+        for(int i = 1;i <= maximumElement(piles);i++){
+            int k = i;
+            int hours = totalHours(piles,k);
+            if(hours <= h) return i;
+    }
 }
 
 int main(){
