d5cbe4717ff7eea3c3b62583d1ea37337826b90a
ParthRKhakre <parthrkhakre@gmail.com>
2026-03-26 14:53:44 +0530

    done

--- CPP/5.1-BinarySearch/BS On Answer/2-NthSqrt.cpp
+++ CPP/5.1-BinarySearch/BS On Answer/2-NthSqrt.cpp
@@ -9,11 +9,38 @@ int nthsqrt(int n,int m){
         if(val == m) ans = i; 
         else if(val > m) break;
     }
+    return ans;   
+}
+
+//  FAST EXPONENTIAL 
+long long power(int val,int n,int m){
+    long long pow_val = 1;
+    while(n > 0){
+        if(n%2 == 1){
+            pow_val *= val;
+            if(pow_val > m) return pow_val; 
+        }
+        val *= val;
+        if(val > m) val = m + 1;
+        n /= 2;
+    }
+    return pow_val;
+}
+
+int nthsqrtbs(int n,int m){
+    int ans = -1,low = 1,high = m;
+    while(low <= high){
+        int mid = (low + high) /2;
+        long long val = power(mid,n,m);
+        if(val == m) return mid;
+        else if(val < m) low = mid + 1;
+        else high = mid - 1; 
+    }
     return ans;
 }
 
 int main(){
     int n,m;
     cin >> n >> m;
-   cout << nthsqrt(n,m);
+   cout << nthsqrtbs(n,m);
 }
