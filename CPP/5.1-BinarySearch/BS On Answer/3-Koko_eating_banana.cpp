85018c4b412c1226b75a97f70228e82ad243a5cb
ParthRKhakre <parthrkhakre@gmail.com>
2026-03-26 16:19:47 +0530

    done

--- CPP/5.1-BinarySearch/BS On Answer/3-Koko_eating_banana.cpp
+++ CPP/5.1-BinarySearch/BS On Answer/3-Koko_eating_banana.cpp
@@ -1,8 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int koko(vector<int>&num,int h){
 
+    
+
+}
 
 int main(){
+
+    int n,h;
+    cin >> n >> h;
+
+    vector<int> num(n);
+
+    for(int i = 0;i < n;i++)
+        cin>>num[i];
+       
+    cout<< koko(num,h);    
 
 }
