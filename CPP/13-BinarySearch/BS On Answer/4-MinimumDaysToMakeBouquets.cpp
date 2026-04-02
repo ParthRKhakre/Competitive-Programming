b4a0146b4241491f32416a4406bb7aff8c80affe
ParthRKhakre <parthrkhakre@gmail.com>
2026-04-02 15:10:53 +0530

    done

---
+++ CPP/13-BinarySearch/BS On Answer/4-MinimumDaysToMakeBouquets.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int M_Bouquets(vector<int> bloomdays,int m,int k){
+    int mx = *max_element(bloomdays.begin(),bloomdays.end());
+    int mn = *min_element(bloomdays.begin(),bloomdays.end());
+
+    while()
+}
+
+int main(){
+
+}
