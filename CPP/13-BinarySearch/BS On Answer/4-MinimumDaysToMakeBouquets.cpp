61cba7184f1950f721c56e0b0542ada477eef807
ParthRKhakre <parthrkhakre@gmail.com>
2026-04-02 16:53:26 +0530

    minimum bouquest

--- CPP/13-BinarySearch/BS On Answer/4-MinimumDaysToMakeBouquets.cpp
+++ CPP/13-BinarySearch/BS On Answer/4-MinimumDaysToMakeBouquets.cpp
@@ -1,13 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int M_Bouquets(vector<int> bloomdays,int m,int k){
+bool checker(vector<int> bloomDay,int m,int k,int day){
+    int cnt = 0,bouquets = 0;
+    for(int i = 0;i < bloomDay.size();i++){
+        if(bloomDay[i]<=day) cnt++;
+        else{
+            bouquets += (cnt / k);
+            cnt = 0;
+        }
+    }
+    bouquets += (cnt / k);
+    if(m <= bouquets) return true;
+    else return false;
+}
+
+int minimumDays(vector<int> bloomdays,int m,int k){
     int mx = *max_element(bloomdays.begin(),bloomdays.end());
     int mn = *min_element(bloomdays.begin(),bloomdays.end());
 
-    while()
+    if((long long)m*k > bloomdays.size()) return -1;
+    for(int i = mn;i<=mx;i++)
+        if(checker(bloomdays,m,k,i)) return i;
+
+    return -1;
+}
+
+int optimized(vector<int> bloomdays,int m,int k){
+    int high = *max_element(bloomdays.begin(),bloomdays.end());
+    int low = *min_element(bloomdays.begin(),bloomdays.end());
+
+    if((long long)m*k > bloomdays.size()) return -1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(checker(bloomdays,m,k,mid)) 
+        high = mid - 1;
+        else low = mid + 1;
+    }
+    return low;
 }
 
 int main(){
-
+    int n,m,k;
+    cin >> n >> m >> k;
+    vector<int> bloomDay(n);
+    for(int i = 0;i<n;i++)
+    cin >> bloomDay[i];
+    cout<< optimized(bloomDay,m,k);
 }
