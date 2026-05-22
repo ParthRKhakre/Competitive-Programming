4d79a98fbe9c43938fbdfa5af326aec1d29804d6
ParthRKhakre <parthrkhakre@gmail.com>
2026-05-22 23:56:51 +0530

    CP

--- Codeforces/Basics/1_Special.cpp
+++ Codeforces/Basics/1_Special.cpp
@@ -25,8 +25,28 @@ int main(){                                                                     
      \ (blackslash) is called the escape character
      \n means new line escape character
      \t means simple tab character
-     \0 its a null character primary use is to mark the end of a string     */
+     \0 its a null character primary use is to mark the end of a string
+     
+     Variables
+     are the containers that stores specific types of data
+     They can be modified with the assignment operator "="
 
+     Syntax: datatype  identifier/name = value;
+
+     Rules to name variables:
+     It cannot have spaces use underscore(_)
+     It cannot start with digit 
+     Cannot have a keyword as variable name
+     Cannot have same name taken by Another variable in the samee scope
+                                                                            */
+    int x = 9;
+    x = 10;
+    cout << x << endl;
+
+    int y; // Undefined value might give an error
+    cout << y << endl;
+
+    
 
        
 }
--- Codeforces/Basics/2_Operators.cpp
+++ Codeforces/Basics/2_Operators.cpp
@@ -3,12 +3,44 @@ using namespace std;
 int main(){
                                                              /*
     Arithematic Operator 
+    It uses BODMAS Rules
     + (Addition)
     - (Subtraction)
     * (Multiplication)
     / (Division)
     % (Modulus)                                              */
 
-    
-    
+    int a,b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+
+    cout << a / b << endl;
+    // dividing  two integer always gives back another integer.
+
+    cout << a % b << endl;
+
+    cout << (int)'a'<<endl;
+    // Typecasting - convert one datatype into another
+
+    cout << (char)'a' + 1 << endl;
+
+    // Garbage Values (undefined values)
+    cout << 'aa++' << endl;                                                     /*
+
+ Precedence of the datatype:(Low to high)
+ bool
+ char / signed char / unsigned char
+ short
+ int
+ long
+ long long
+ float
+ double
+ long double  
+                                                                                */
+
+
 }
